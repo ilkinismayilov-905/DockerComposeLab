4033b51b4e4bc7449cbc80fb5e4fa025caedfde2
Ilkin Ismayilov <ismayilovilkin2006@gmail.com>
2026-05-07 14:27:31 +0400

    first commit

---
+++ src/main/java/org/example/dockercomposelab/DockerComposeLabApplication.java
@@ -0,0 +1,13 @@
+package org.example.dockercomposelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerComposeLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerComposeLabApplication.class, args);
+    }
+
+}
